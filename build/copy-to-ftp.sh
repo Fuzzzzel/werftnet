5d97272e1b284838e367056b628db1c6e9ff3e3e
Fuzzzzel <the_weirdo@gmx.net>
2018-05-13 04:45:12 +0200

    Wrong syntyx for ftp connection

--- build/copy-to-ftp.sh
+++ build/copy-to-ftp.sh
@@ -1,4 +1,4 @@
-ftp -inv ftp://web96f1@${WERFTNET_BUILD_FTP_KEY}:server23.webgo24.de << EOT
+ftp -inv ftp://web96f1:${WERFTNET_BUILD_FTP_KEY}@server23.webgo24.de << EOT
 put logs/coveralls-upload.json
 bye
 EOT
