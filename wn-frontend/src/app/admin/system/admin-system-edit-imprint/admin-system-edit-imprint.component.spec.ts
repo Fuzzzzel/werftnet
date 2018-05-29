0f0b39cf8e9ab490c61d8ed843e37db0c5a5bb0b
Fuzzzzel <the_weirdo@gmx.net>
2018-05-29 09:48:53 +0200

    Add frontend save imprint tests (not finished)

--- wn-frontend/src/app/admin/system/admin-system-edit-imprint/admin-system-edit-imprint.component.spec.ts
+++ wn-frontend/src/app/admin/system/admin-system-edit-imprint/admin-system-edit-imprint.component.spec.ts
@@ -5,11 +5,12 @@ import { SharedModule } from '../../../shared/shared.module';
 import { AdminSystemEditImprintService } from './admin-system-edit-imprint.service';
 import { UtilService } from '../../../core/util.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('AdminSystemEditImprintComponent', () => {
   let component: AdminSystemEditImprintComponent;
   let fixture: ComponentFixture<AdminSystemEditImprintComponent>;
+  let backend: HttpTestingController
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,11 +21,13 @@ describe('AdminSystemEditImprintComponent', () => {
       ],
       providers: [
         AdminSystemEditImprintService,
+        HttpTestingController,
         UtilService
       ],
       declarations: [AdminSystemEditImprintComponent]
     })
       .compileComponents();
+    backend = TestBed.get(HttpTestingController)
   }));
 
   beforeEach(() => {
@@ -36,4 +39,30 @@ describe('AdminSystemEditImprintComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should cancel saving', () => {
+    component.cancelSaveImprint()
+  });
+
+  xit('should save imprint', () => {
+    component.imprint = 'Imprint Text'
+    component.saveImprint()
+
+    const req2 = backend.expectOne('/admin/settings/imprint')
+    expect(req2.request.method).toBe("POST")
+    req2.flush('New Imprint Text', { status: 200, statusText: 'OK' })
+  })
+
+  it('should fail to save imprint', () => {
+    component.imprint = 'Imprint Text'
+    component.saveImprint()
+
+    spyOn(window, 'alert').and.returnValue(true);
+
+    /*
+    const req = backend.expectOne('/admin/settings/imprint')
+    expect(req.request.method).toBe("POST")
+    req.flush('New Imprint Text', { status: 404, statusText: '404 Not Found' })
+    */
+  })
 });
